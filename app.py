b8a8620b44c5446d070c110dff617b3a7bf37eb0
joselucianocantejimenz-sketch <joselucianocantejimenz@gmail.com>
2026-09-02 14:01:30 -0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,5 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return "<h1>VIAJES ESTRELLA Buctzotz</h1><p>Merida $150 | Cancun $600 | Chichen $300</p><a href='https://wa.me/529861234567'>Reservar</a>"
